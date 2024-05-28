9ca4586ecf4640341cac865ae4d8821a031cc7a1
Paula Bigalke <paula.bigalke@uni-koeln.de>
2024-05-28 17:28:38 +0200

    prepare first part of exercise - data preprocessing.

--- data_processing/load_datasets.py
+++ data_processing/load_datasets.py
@@ -1,78 +1,97 @@
 # %%
 import tensorflow as tf
 import glob
-import numpy as np
-from preprocess_methods import normalize, resize, random_jitter
+
+IMAGE_SIZE = 128
 
 # %%
-def get_paired_IR_VIS_file_lists(path):
-    # get all ir images from path
-    ir_images = sorted(glob.glob(f"{path}/*IR_108.png"))
+def _get_paired_IR_VIS_file_lists(path):
+    # get all ir image files from path
+    ir_image_list = glob.glob(f"{path}/*IR_108.png")
+    # get corresponding vis image files
+    vis_image_list = [ir.replace("IR_108", "VIS006") for ir in ir_image_list]
+    # return matched lists
+    return ir_image_list, vis_image_list
 
-    # get corresponding vis images
-    vis_images = [ir.replace("IR_108", "VIS006") for ir in ir_images]
-
-    return ir_images, vis_images
-
-def load_single_image(image_file):
+def _load_single_image(image_file):
     # read in image
     image = tf.io.read_file(image_file)
     image = tf.image.decode_png(image, channels=1)
     return image
 
+def _resize(ir_image, vis_image):
+    # ensures that all images have the correct size
+    ir_image = tf.image.resize(ir_image, [IMAGE_SIZE, IMAGE_SIZE], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    vis_image = tf.image.resize(vis_image, [IMAGE_SIZE, IMAGE_SIZE], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return ir_image, vis_image
+
+def _normalize(ir_image, vis_image):
+    # normalize the images to range [-1..1]
+    ir_image = (tf.cast(ir_image, tf.float32) / 127.5) - 1.0 
+    vis_image = (tf.cast(vis_image, tf.float32) / 127.5) - 1.0 
+    return ir_image, vis_image
+
+def _random_jitter(ir_image, vis_image):
+    # randomly flip the images left-right
+    if tf.random.uniform(()) > 0.5:
+        ir_image = tf.image.flip_left_right(ir_image)
+        vis_image = tf.image.flip_left_right(vis_image)
+    return ir_image, vis_image
+
 # %%
-def preprocess_train_images(ir_image_file, vis_image_file):
+def _preprocess_train_images(ir_image_file, vis_image_file):
 
     # load images as tf.tensor
-    ir_image = load_single_image(ir_image_file)
-    vis_image = load_single_image(vis_image_file)
+    ir_image = _load_single_image(ir_image_file)
+    vis_image = _load_single_image(vis_image_file)
 
-    ##### do some preprocessing #####
-    ir_image, vis_image = resize(ir_image, vis_image)
-    ir_image, vis_image = random_jitter(ir_image, vis_image)
-    ir_image, vis_image = normalize(ir_image, vis_image)
+    # do some preprocessing
+    ir_image, vis_image = _resize(ir_image, vis_image)
+    ir_image, vis_image = _random_jitter(ir_image, vis_image)
+    ir_image, vis_image = _normalize(ir_image, vis_image)
 
     return ir_image, vis_image
 
-def load_train_dataset(train_path, batch_size):
+def load_train_dataset(train_path, batch_size, buffer_size=100):
 
     # get all ir and vis images from path, sorted into paired lists
-    train_ir_images, train_vis_images = get_paired_IR_VIS_file_lists(train_path)
+    train_ir_images, train_vis_images = _get_paired_IR_VIS_file_lists(train_path)
 
-    # create dataset
+    # create tensorflow dataset object from list of images 
     train_dataset = tf.data.Dataset.from_tensor_slices((train_ir_images, train_vis_images))
 
-    # preprocess images
-    train_dataset = train_dataset.map(preprocess_train_images)
+    # preprocess images. The map function of tensorflow calls _preprocess_train_images on each element
+    train_dataset = train_dataset.map(_preprocess_train_images)
 
-    # organize dataset into batches
-    train_dataset = train_dataset.shuffle(10).batch(batch_size)
+    # organize dataset into batches after shuffling
+    train_dataset = train_dataset.shuffle(buffer_size).batch(batch_size)
+
 
     return train_dataset
 
 # %%
-def preprocess_test_images(ir_image_file, vis_image_file):
+def _preprocess_test_images(ir_image_file, vis_image_file):
 
     # load images as tf.tensor
-    ir_image = load_single_image(ir_image_file)
-    vis_image = load_single_image(vis_image_file)
+    ir_image = _load_single_image(ir_image_file)
+    vis_image = _load_single_image(vis_image_file)
 
-    ##### do some preprocessing #####
-    ir_image, vis_image = resize(ir_image, vis_image)
-    ir_image, vis_image = normalize(ir_image, vis_image)
+    # do some preprocessing
+    ir_image, vis_image = _resize(ir_image, vis_image)
+    ir_image, vis_image = _normalize(ir_image, vis_image)
     
     return ir_image, vis_image
 
 def load_test_dataset(test_path, batch_size):
 
     # get all ir and vis images from path, sorted into paired lists
-    test_ir_images, test_vis_images = get_paired_IR_VIS_file_lists(test_path)
+    test_ir_images, test_vis_images = _get_paired_IR_VIS_file_lists(test_path)
 
     # create dataset
     test_dataset = tf.data.Dataset.from_tensor_slices((test_ir_images, test_vis_images))
 
     # preprocess images
-    test_dataset = test_dataset.map(preprocess_test_images)
+    test_dataset = test_dataset.map(_preprocess_test_images)
 
     # organize dataset into batches
     test_dataset = test_dataset.batch(batch_size)
